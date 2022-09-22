174ff115a33265659c344441fda890bce445d672
JoaquinLeon <joaquinleondev@gmail.com>
2022-09-22 00:16:01 -0300

    Version 1.0

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
 
 const NavBar = () => {
-  const items = ['Inicio', 'Sobre nosotros', 'Carrito']
   return (
     <>
-      {items.map((item) => (
-        <li>{item}</li>
-      ))}
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MarketLink</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Instagram</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tienda
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Catalogo</a></li>
+            <li><a class="dropdown-item" href="#">Carrito</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Tus compras</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Seguimiento (Proximamente)</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Busca algun producto" aria-label="Search" />
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
+    
     </>
   )
 }
